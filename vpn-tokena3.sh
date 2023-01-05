b0a0aa440d04e165fb4cc11bcc4e60dc96c86ed7
Cesar de Andrade Cardoso <cesar.andrade@dataprev.gov.br>
2023-01-05 13:18:39 -0300

    Modificando o script de conexão à VPN usando token A3 para não precisar rodar como root, usando sudo apenas quando necessário.

--- vpn-tokena3.sh
+++ vpn-tokena3.sh
@@ -15,9 +15,8 @@ SERVERCERT="certificado-do-servidor"
 CAFILE="/local/da/cadeia/do/token/a3.pem"
 PORTAL="portal.vpn.com.br"
 USUARIO="usuario@servidor.com.br"
-# Descomente a variável GATEWAY para o authgroup desejado
-#GATEWAY="GW_EXT_DCSP_A3"
-GATEWAY="GW_EXT_DCDF_A3"
+# Modifique a variável GATEWAY para o authgroup desejado
+GATEWAY="GW_EXT_AUTHGROUP_TOKEN_A3"
 # Estas variáveis não são necessárias caso a conexão VPN sete automaticamente os DNS
 DNS1=0.0.0.0
 DNS2=0.0.0.0
@@ -41,21 +40,7 @@ function help {
 	echo
 }
 
-function check_root {
-	#
-	# Checando se estamos rodando como root
-	#
-	echo "Checando se estamos rodando como root..."
-	if [[ $(id -u) -ne 0 ]]
-	then
-  		echo "Este script precisa rodar como root!"
- 		echo "Use o comando \"sudo\" para executar este script."
- 		exit 1
-	fi
-}
-
 function connect {
-	check_root
 	#
 	# Testa se o token está plugado na USB
 	# Se o token não estiver plugado na USB, avisa e sai sem conectar
@@ -71,38 +56,39 @@ function connect {
 
 	#
 	# Se o token estiver plugado, conecta com o Openconnect
-	# O utilizador é alertado que precisa do PIN do token *e* da senha LDAP 
+	# O utilizador é alertado que precisa do PIN do token *e* da senha de login
+	# Usamos o sudo para não precisar rodar o script todo como root
 	#
 	echo "*** ATENÇÃO ***"
 	echo "Entre com o PIN do token e a senha de login."
-	openconnect --authgroup $GATEWAY --certificate $USERCERT --servercert $SERVERCERT --protocol=gp --cafile=$CAFILE --disable-ipv6 --syslog --pid-file=$PID --background $PORTAL --user $USUARIO
+	sudo openconnect --authgroup $GATEWAY --certificate $USERCERT --servercert $SERVERCERT --protocol=gp --cafile=$CAFILE --disable-ipv6 --syslog --pid-file=$PID --background $PORTAL --user $USUARIO
 
-    #
-    # Já que a saída está toda no syslog, precisamos checar se *realmente* estamos conectados.
-    # Para isso, precisamos esperar a VPN estabilizar (ou não)
-    #
-    echo "Esperando a VPN estabilizar... Aguarde!"
-    sleep 30 
-    ENDIP=$(ip a show dev tun0 2>/dev/null | grep -w "inet" | cut -d " " -f 6 | cut -d "/" -f 1)
-    if [[ -z $ENDIP ]]
-    then
-            echo "Não consegui identificar um endereço IP! Verifique se estamos logados na VPN."
-            exit 1
-    else
-            echo "Estamos conectados à VPN com o IP "$ENDIP
-    fi
+	#
+        # Já que a saída está toda no syslog, precisamos checar se *realmente* estamos conectados.
+	# Para isso, precisamos esperar a VPN estabilizar (ou não)
+	#
+	echo "Esperando a VPN estabilizar... Aguarde!"
+	sleep 10 
+	ENDIP=$(ip a show dev tun0 2>/dev/null | grep -w "inet" | cut -d " " -f 6 | cut -d "/" -f 1)
+	if [[ -z $ENDIP ]]
+	then
+		echo "Não consegui identificar um endereço IP! Verifique se estamos logados na VPN."
+		exit 1
+	else
+		echo "Estamos conectados à VPN com o IP "$ENDIP
+	fi
 
 	#
 	# Apontamos os DNS e os resolvers de domínios de busca que queremos para a conexão da VPN (tun0)
+	# Precisamos disso porque o openconnect não faz isso automaticamente
 	#
 	# Dica retirada de https://www.gabriel.urdhr.fr/2020/03/17/systemd-revolved-dns-configuration-for-vpn/
 	#
-	resolvectl dns tun0 $DNS1 $DNS2
-	resolvectl domain tun0 $DOMAIN1 $DOMAIN2
+	sudo resolvectl dns tun0 $DNS1 $DNS2
+	sudo resolvectl domain tun0 $DOMAIN1 $DOMAIN2
 }
 
 function disconnect {
-	check_root
 	#
 	# Checando para saber se a VPN está rodando pelo arquivo PID
 	#
@@ -115,11 +101,12 @@ function disconnect {
 	fi
 	#
 	# Se estiver rodando, fecha a conexão com um SIGINT para o processo
+	# Usamos o sudo para matar o processo, já que a VPN rodou como root via sudo.
 	#
 	echo
 	echo "Encerrando a VPN"
 	PIDNUM=$(cat $PID)
-	kill -SIGINT $PIDNUM
+	sudo kill -SIGINT $PIDNUM
 	sleep 10
 	echo "Desconectados!"
 }
